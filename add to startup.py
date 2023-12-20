c367be731fe6df90cb93031779c825d4c1990a65
iLLokMaster <romapridieb@gmail.com>
2023-12-20 14:06:55 +0300

    Create add to startup.py

---
+++ add to startup.py
@@ -0,0 +1,12 @@
+import os
+import getpass
+USER_NAME = getpass.getuser()
+
+def add_to_startup(file_path="c://download//main.py"): #c://download//main.py
+    # if file_path == "":
+    #     file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % file_path)
+
+add_to_startup()
